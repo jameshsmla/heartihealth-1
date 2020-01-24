d6c56b2bc2b423547a82963e9babf48f5927e12e
james <james@pratian.com>
2020-01-24 17:16:19 +0530

    asm

--- discoveri-heartihealth-webapp/src/main/java/com/discoveri/heartihealth/web/HeartiApplication.java
+++ discoveri-heartihealth-webapp/src/main/java/com/discoveri/heartihealth/web/HeartiApplication.java
@@ -9,17 +9,4 @@ public class HeartiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HeartiApplication.class, args);
 	}
-
-	/*
-	 * @RestController static class SomeRestController {
-	 * 
-	 * @Autowired private PatientInfoServiceImple someService;
-	 * 
-	 * @RequestMapping(value = "/") public ResponseEntity<Patient> getPatientInfo()
-	 * throws SQLException { try { Patient patient = someService.getPatientInfo();
-	 * if (patient == null) { return new ResponseEntity<Patient>(patient,
-	 * HttpStatus.NOT_FOUND); } else return new ResponseEntity<Patient>(patient,
-	 * HttpStatus.OK); } catch (PatientExceptions e) { // TODO Auto-generated catch
-	 * block e.printStackTrace(); } return null; } }
-	 */
 }
